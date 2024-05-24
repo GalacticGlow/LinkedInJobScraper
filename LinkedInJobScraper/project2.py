d0d9aac19ae8f8320ab6401ccaab1298f820cf16
GalacticGlow <art.gulidov@gmail.com>
2024-05-24 23:19:51 +0300

    dssadfsd

--- LinkedInJobScraper/project2.py
+++ LinkedInJobScraper/project2.py
@@ -16,6 +16,12 @@ driver = webdriver.Chrome(service=service, options=options)
 
 driver.get(url)
 
+job_not_found_message_xpath = '//h1[contains(@class, "core-section-container__main-title main-title")]'
+location_not_found_xpath = '//h2[contains(@class, "authwall-sign-in-form__header-title")]'
+job_found = True
+
+yankee_list = ['united states of america', 'us', 'usa', 'america', 'united states']
+
 job_name = input('What kind of job are you looking for?')
 job_location = input('Where would you like your job to be located?')
 
@@ -30,20 +36,24 @@ while True:
     job_location_field.clear()
     job_location_field.send_keys(job_location)
 
+    job_search_bar.clear()
     job_search_bar.send_keys(job_name)
     job_search_bar.send_keys(Keys.ENTER)
 
     try:
-        no_result_message_xpath = '//h1[contains(@class, "core-section-container__main-title main-title")]'
-        if len(driver.find_elements(By.XPATH, no_result_message_xpath)) > 0:
+        if driver.find_element(By.XPATH, '//input[contains(@value, "United States")]').is_displayed() and job_location not in yankee_list:        
+            print("Hmmm, it seems like we couldn't find a country called " + job_location + ". Please check the spelling and try again:")
+        elif len(driver.find_elements(By.XPATH, job_not_found_message_xpath)) > 0:
             print("Hmmm, it seems like we couldn't find a match for " + job_name + ". Please check the spelling and try again:")
+            job_found = False
         else:
             break
     except NoSuchElementException:
         print('Something went wrong')
     
-    job_name = input('What kind of job are you looking for?')
+    if not job_found:
+        job_name = input('What kind of job are you looking for?')
     job_location = input('Where would you like your job to be located?')
 
-time.sleep(3)
+time.sleep(5)
 driver.quit()
